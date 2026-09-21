19f736216fbcfa28a846f9910dcdd77c33a53605
mohit3500 <yadavmohit3147@gmail.com>
2026-09-21 15:34:26 +0530

    testing5 commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: [
+    "firebase-admin",
+    "jwks-rsa",
+    "jose",
+  ],
+
   images: {
     remotePatterns: [
       {
