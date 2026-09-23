8445ae89c9b0a682ba6a85e6acd7bde942438e51
mohit3500 <yadavmohit3147@gmail.com>
2026-09-23 12:58:00 +0530

    Medieval10-12 commit

--- lib/all-tests.ts
+++ lib/all-tests.ts
@@ -1,17 +1,23 @@
 import type { Test } from "@/lib/types";
 
-import { tests1 } from "@/lib/History1-8";
-import { tests2 } from "@/lib/History9-15";
+import { test1 } from "@/lib/History1-8Part1";
+import { test7 } from "@/lib/History1-8Part2";
+import { test2 } from "@/lib/History9-15Part1";
 import { test3 } from "@/lib/Medieval1-3"
 import { test4 } from "@/lib/Medieval4-6"
 import { test5 } from "@/lib/Medieval7-9"
+import { test6 } from "@/lib/Medieval10-12"
+import { test8 } from "@/lib/History9-15Part2"
 
 export const allTests: Test[] = [
-    ...tests1,
-    ...tests2,
+    ...test1,
+    ...test7,
+    ...test2,
+    ...test8,
     ...test3,
     ...test4,
     ...test5,
+    ...test6,
 ];
 
 export function getTestById(id: string): Test | undefined {
